248bcbe3025a725c21084d8b12a04398b51e11c4
Fabio <fabio@tiva.app>
2023-01-11 15:27:28 -0300

    Create interface for create new user

--- src/@types/global.d.ts
+++ src/@types/global.d.ts
@@ -1,8 +1,19 @@
+declare interface UserCreateInputs {
+    email: string,
+    name: string,
+    cpf_cnpj: string,
+    password?: string,
+    password_confirmation?: string,
+    encrypted_password?: string
+}
+
+type UserFindKeys = "id" | "name" | "cpf_cnpj" | "email";
 declare interface HTTPRequestObject {
     id?: string,
     sender?: string,
     receiver?: string,
-    total_cents?: number
+    total_cents?: number,
+    user?: UserCreateInputs
 }
 declare interface TransferCreateInputs {
     total_cents: number,
